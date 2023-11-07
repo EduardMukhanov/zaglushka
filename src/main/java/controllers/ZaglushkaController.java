56a9022c9b361474fd33dbd8e0d1ca03871aae03
EduardMukhanov <emfo61933@gmail.com>
2023-11-07 17:13:31 +0300

    Add check json

--- src/main/java/controllers/ZaglushkaController.java
+++ src/main/java/controllers/ZaglushkaController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.ZaglushkaUser;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,9 @@ public class ZaglushkaController {
 
     @PostMapping("/postEndPoint")
     public ResponseEntity<?> postEndPoint(@RequestBody ZaglushkaUser User){
+        if (User.getLogin().isEmpty() || User.getPassword().isEmpty()){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(500));
+        }
         User.setDate(LocalDate.now());
         return new ResponseEntity<>(User, HttpStatus.OK);
     }
